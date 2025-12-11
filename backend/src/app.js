4f9e80daef63e3a854c35296e6abf7fd36df792a
Rubi <rubino06@hotmail.fr>
2025-12-11 21:50:31 +0100

    Ajout du README complet + corrections finales

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
-import rateLimit from "express-rate-limit";
+// import rateLimit from "express-rate-limit";
 import { sequelize } from "./config/db.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
@@ -21,7 +21,7 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(rateLimit({ windowMs: 60 * 1000, limit: 100 }));
+// app.use(rateLimit({ windowMs: 60 * 1000, limit: 100 }));
 
 // CORS ouvert à tout le monde (suffisant pour ton devoir)
 app.use(cors());
